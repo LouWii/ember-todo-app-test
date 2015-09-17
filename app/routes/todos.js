537b15eee93adedee79c434c8b7e54cddb07494e
louwii <link14.online@gmail.com>
2015-09-16 23:57:24 -0700

    Steps done until Create a new todo

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,19 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // var todos = this.store.findAll('todo'); // Adapter operation failed
-    // var todos = [];
-    // console.log(todos);
-    let todos = [
-      {
-        title: "Learn Ember",
-        complete: false,
-      },
-      {
-        title: "Solve World Hunger",
-        complete: false,
-      }
-    ];
+    var todos = this.store.findAll('todo'); // Adapter operation failed
+    console.log(todos);
+    // let todos = [
+    //   {
+    //     title: "Learn Ember",
+    //     complete: false,
+    //   },
+    //   {
+    //     title: "Solve World Hunger",
+    //     complete: false,
+    //   }
+    // ];
     return todos;
   }
 });
